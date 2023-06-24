c7956b3f3a9c658e89f07e4bd53da0e7e5fb3b51
twin <twin@twin.com>
2023-06-24 05:42:25 -0600

    implement exllama inference

--- config.py
+++ config.py
@@ -1,15 +1,9 @@
-from auto_gptq import BaseQuantizeConfig
-from transformers import GenerationConfig
-
-model_name = "TheBloke/guanaco-65B-GPTQ"
-
-model_basename = "Guanaco-65B-GPTQ-4bit.act-order"
-
-generation_config = GenerationConfig(
-        num_beams=3,
-        do_sample=True,
-        max_new_tokens=1000,
-        early_stopping=True,
-        temperature=0.1,
-        top_k=50,
-    )
+repo_name = "TheBloke/guanaco-7B-GPTQ"
+model_name = "guanaco-7B-GPTQ"
+model_basename = "Guanaco-7B-GPTQ-4bit-128g.no-act-order"
+max_new_tokens = 1000
+token_repetition_penalty_max = 1.2
+temperature = 0.1
+top_p = 1
+top_k = 40
+typical = 0.5
