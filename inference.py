8419100e417564b0f88c22465fd31b8ed816f4e4
twin <twin@twin.com>
2023-06-24 06:25:02 -0600

    download model first

--- inference.py
+++ inference.py
@@ -7,14 +7,14 @@ from generator import ExLlamaGenerator
 
 class Predictor:
     def setup(self):
+        # Download model
+        snapshot_download(repo_id=repo_name, local_dir=model_directory)
+        
         model_directory = f"/data/{model_name}"
         tokenizer_path = os.path.join(model_directory, "tokenizer.model")
         model_config_path = os.path.join(model_directory, "config.json")
         st_pattern = os.path.join(model_directory, "*.safetensors")
         model_path = glob.glob(st_pattern)[0]
-        
-        # Download model
-        snapshot_download(repo_id=repo_name, local_dir=model_directory)
         
         config = ExLlamaConfig(model_config_path)               # create config from config.json
         config.model_path = model_path                          # supply path to model weights file
