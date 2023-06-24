017bea74eb091656fa13d7582509edcf4bf963cb
twin <twin@twin.com>
2023-06-24 06:28:17 -0600

    sad face

--- inference.py
+++ inference.py
@@ -8,9 +8,9 @@ from generator import ExLlamaGenerator
 class Predictor:
     def setup(self):
         # Download model
+        model_directory = f"/data/{model_name}"
         snapshot_download(repo_id=repo_name, local_dir=model_directory)
         
-        model_directory = f"/data/{model_name}"
         tokenizer_path = os.path.join(model_directory, "tokenizer.model")
         model_config_path = os.path.join(model_directory, "config.json")
         st_pattern = os.path.join(model_directory, "*.safetensors")
